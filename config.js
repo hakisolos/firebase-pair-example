3a91f77e60104eb440e652d7769f62f16eaddca8
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-04-11 21:29:16 +0100

    GOD DID

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /** @format */
 
 module.exports = {
-	FIREBASE_DATABASE_URL: 'https://pair-example-default-rtdb.firebaseio.com/', // replace with yours
+	FIREBASE_DATABASE_URL: '', // replace with yours
 	FIREBASE_STORAGE_BUCKET: '...', // you can ignore if not using
 	PREFIX: 'SESSION-ID-',
 	IMAGE: 'https://files.catbox.moe/lt53tx.jpg',
